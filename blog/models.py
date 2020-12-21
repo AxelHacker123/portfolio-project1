403a6b1c837513e3d6d933f1bf4177c2d5a7da3c
austin <am127438@live.com>
2020-12-21 10:29:25 -0800

    idkman

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class blog(models.Model):
-    image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+class Blog(models.Model):
+    title = models.CharField(max_length=255, default = '')
+    pub_date = models.DateTimeField(blank=True, null=True)
+    body = models.TextField(default='')
+    image = models.ImageField(upload_to='images/', default="")
